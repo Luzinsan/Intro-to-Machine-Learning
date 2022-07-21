c40cba083f4561885a00b1a6ec2797cef08e02b9
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2022-07-21 15:19:09 +0700

    Update DecisionTreeRegressor.py

--- DecisionTreeRegressor.py
+++ DecisionTreeRegressor.py
@@ -1,3 +1,5 @@
+# API: 
+# kaggle kernels output dansbecker/underfitting-and-overfitting -p /path/to/dest
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
